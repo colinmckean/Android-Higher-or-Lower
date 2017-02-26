31370468db58743039f9c0cd860ce3ecaff96659
Colin McKean <colinmckeandigital@gmail.com>
2017-02-26 20:55:16 +0000

    Refactor random number generation to a seperate function.

--- app/src/main/java/com/codeclan/example/higherorlower/MainActivity.java
+++ app/src/main/java/com/codeclan/example/higherorlower/MainActivity.java
@@ -1,7 +1,7 @@
 package com.codeclan.example.higherorlower;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,30 +10,36 @@ import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
-    private int randdomNumber;
-    public void makeToast(String s){
+    private int randomNumber;
+    private Random rand;
 
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-    }
-    public void guess(View view){
-        EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
-        int guessInt = Integer.parseInt(guessEditText.getText().toString());
-        if (guessInt > randdomNumber){
-            makeToast("Lower!");
-
-        }else if (guessInt < randdomNumber ){
-            makeToast("Higher");
-        }else{
-            makeToast("That's right! Have Another Go!");
-            Random rand = new Random();
-            randdomNumber = rand.nextInt(20)+1;
-        }
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Random rand = new Random();
-        randdomNumber = rand.nextInt(20)+1;
+        randomNum();
+    }
+
+    private int randomNum() {
+        rand = new Random();
+        randomNumber = rand.nextInt(20) + 1;
+        return randomNumber;
+    }
+
+    public void makeToast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    public void guess(View view) {
+        EditText guessEditText = (EditText) findViewById(R.id.guessEditText);
+        int guessInt = Integer.parseInt(guessEditText.getText().toString());
+        if (guessInt > randomNumber) {
+            makeToast("Lower!");
+        } else if (guessInt < randomNumber) {
+            makeToast("Higher");
+        } else {
+            makeToast("That's right! Have Another Go!");
+            randomNum();
+        }
     }
 }
